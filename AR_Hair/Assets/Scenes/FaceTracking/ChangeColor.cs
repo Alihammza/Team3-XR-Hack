a0d63ae2ba8201fb85f3370c082e8a19ef4ae1de
Alihammza <alihamza1010@gmail.com>
2019-10-20 13:21:08 +0200

    First commit

---
+++ AR_Hair/Assets/Scenes/FaceTracking/ChangeColor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeColor : MonoBehaviour
+{
+    public GameObject hairs;
+    [SerializeField]
+    public List<Material> ListOfMaterials;
+    public void ChangeColors()
+    {
+        hairs = GameObject.Find("FaceMeshPrefab");
+        if (hairs != null)
+            hairs.GetComponentInChildren<MeshRenderer>().material = ListOfMaterials[1];
+        else
+            GameObject.Find("Butt1").GetComponentInChildren<Text>().text = "no prefab found";
+    }
+}
+
+
+   
---
+++ Customer_app/Assets/Scenes/ChangeScene.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene0()
+    {
+        SceneManager.LoadScene("Scene0");
+    }
+    public void ChangeScene01()
+    {
+        SceneManager.LoadScene("Scene01");
+    }
+    public void ChangeScene1()
+    {
+        SceneManager.LoadScene("Scene1");
+    }
+    public void ChangeScene11()
+    {
+        SceneManager.LoadScene("Scene11");
+
+    }
+    public void ChangeScene12()
+    {
+        SceneManager.LoadScene("Scene12");
+    }
+    public void ChangeScene13()
+    {
+        SceneManager.LoadScene("Scene13");
+    }
+    public void ChangeScene14()
+    {
+        SceneManager.LoadScene("Scene14");
+    }
+    public void ChangeScene15()
+    {
+        SceneManager.LoadScene("Scene15");
+    }
+    public void ChangeScene2()
+    {
+        SceneManager.LoadScene("Scene2");
+    }
+    public void ChangeScene3()
+    {
+        SceneManager.LoadScene("Scene3");
+    }
+    public void ChangeScene31()
+    {
+        SceneManager.LoadScene("Scene31");
+    }
+    public void ChangeScene4()
+    {
+        SceneManager.LoadScene("Scene4");
+    }
+    public void ChangeScene5()
+    {
+        SceneManager.LoadScene("Scene5");
+    }
+    public void ChangeScene6()
+    {
+        SceneManager.LoadScene("Scene6");
+    }
+
+}
